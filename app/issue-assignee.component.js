d7f08ef5cdfe573e29583b1b822dfe5bc7725bcc
Pavel Galaton <pgalaton@pentalog.fr>
2016-03-23 11:11:34 +0200

    Changed page title

---
+++ app/issue-assignee.component.js
@@ -0,0 +1,42 @@
+System.register(["angular2/core"], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+        return c > 3 && r && Object.defineProperty(target, key, r), r;
+    };
+    var __metadata = (this && this.__metadata) || function (k, v) {
+        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+    };
+    var core_1;
+    var IssueAssigneeComponent;
+    return {
+        setters:[
+            function (core_1_1) {
+                core_1 = core_1_1;
+            }],
+        execute: function() {
+            IssueAssigneeComponent = (function () {
+                function IssueAssigneeComponent() {
+                }
+                __decorate([
+                    core_1.Input(), 
+                    __metadata('design:type', Object)
+                ], IssueAssigneeComponent.prototype, "assignee", void 0);
+                IssueAssigneeComponent = __decorate([
+                    core_1.Component({
+                        selector: 'issue-assignee',
+                        template: "\n    <div class=\"issue-assignee\">\n        <img [src]=\"assignee?.avatarUrls['48x48']\" [title]=\"assignee.displayName\" />\n    </div>\n    ",
+                        styles: ['img {border-radius: 50%}']
+                    }), 
+                    __metadata('design:paramtypes', [])
+                ], IssueAssigneeComponent);
+                return IssueAssigneeComponent;
+            }());
+            exports_1("IssueAssigneeComponent", IssueAssigneeComponent);
+        }
+    }
+});
+//# sourceMappingURL=issue-assignee.component.js.map
